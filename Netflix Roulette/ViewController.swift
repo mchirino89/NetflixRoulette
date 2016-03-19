2663ea7e165eedf3a58448195fd0f9332b1b4e65
Mauricio Chirino <mauricio@3codegeeks.com>
2016-03-19 12:49:02 -0430

    Initial Commit

---
+++ Netflix Roulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Netflix Roulette
+//
+//  Created by Mauricio Chirino on 19/3/16.
+//  Copyright © 2016 Mauricio Chirino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
